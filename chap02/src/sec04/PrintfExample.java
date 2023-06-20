18452a3517474ff4b11dcfbda3e5e871f493c25e
KSJ0314 <132119447+KSJ0314@users.noreply.github.com>
2023-06-20 10:41:16 +0900

    test7

--- chap02/src/sec04/PrintfExample.java
+++ chap02/src/sec04/PrintfExample.java
@@ -7,10 +7,14 @@ public class PrintfExample {
 		int value = 123;
 		System.out.printf("상품의 가격 : %d원\n", value);
 		System.out.printf("상품의 가격 : %6d원\n", value);
-
-		value = 1234;
-		System.out.printf("상품의 가격 : %d원\n", value);
-		System.out.printf("상품의 가격 : %6d원\n", value);
+		
+		double area = 3.14159 *10 *10 ;
+		System.out.printf("반지름이 %d인 원의 넓이 : %10.3f\n", 10, area);
+		
+		String name = "홍길동";
+		String job = "의적";
+		
+		System.out.printf("%6d | %-10s | %10s\n", 1, name, job);
 
 	}
 
