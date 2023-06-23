693da684afbe48237d512ad7eccc560fc3918ec0
KSJ0314 <132119447+KSJ0314@users.noreply.github.com>
2023-06-23 10:04:08 +0900

    0623

---
+++ chap03/src/sec02/CompareOperatorExample2.java
@@ -0,0 +1,18 @@
+package sec02;
+
+public class CompareOperatorExample2 {
+
+	public static void main(String[] args) {
+
+		int v2 = 1;
+		double v3 = 1.0;
+		System.out.println(v2 == v3);
+		System.out.println(v2);
+
+		float v4 = 0.1f;
+		double v5 = 0.1;
+		System.out.println(v4 == v5);
+
+	}
+
+}
