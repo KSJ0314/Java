dc06bc4b07426158352ef4cd4ae9a10fd0767f83
KSJ0314 <132119447+KSJ0314@users.noreply.github.com>
2023-06-23 10:34:29 +0900

    test

--- chap03/src/sec02/ConditionOperationExample.java
+++ chap03/src/sec02/ConditionOperationExample.java
@@ -1,12 +1,22 @@
 package sec02;
 
+import java.util.Scanner;
+
 public class ConditionOperationExample {
 
 	public static void main(String[] args) {
 
-		int score = 85;
-		char grade = (score > 90) ? 'A' : ((score > 80) ? 'B' : 'C');
-		System.out.println(score + "점은 " + grade + "등급입니다.");
+//		int score = 85;
+//		char grade = (score > 90) ? 'A' : ((score > 80) ? 'B' : 'C');
+//		System.out.println(score + "점은 " + grade + "등급입니다.");
+
+		Scanner s = new Scanner(System.in);
+
+		System.out.print("점수를 입력해주세요. ");
+		int score = s.nextInt();
+
+		char grade = (score >= 90) ? '수' : ((score >= 80) ? '우' : '미');
+		System.out.printf("%d점은 \'%s\'등급입니다.\n", score, grade);
 
 	}
 
