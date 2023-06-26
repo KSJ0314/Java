254bc8f83d31c3e4f893e9befb2b088fa2792c2c
KSJ0314 <132119447+KSJ0314@users.noreply.github.com>
2023-06-26 16:51:55 +0900

    0626

--- chap02/src/백준/Main.java
+++ chap02/src/백준/Main.java
@@ -7,8 +7,14 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner s = new Scanner(System.in);
-		
-		
+
+		int N = s.nextInt();
+
+		for (int i = 0; i < N / 4; i++) {
+			System.out.print("long ");
+		}
+		System.out.println("int");
+
 	}
 
 }
