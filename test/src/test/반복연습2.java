45d7b4f9bf44e1cc6c301bf2fe4f1ecb64ab0ce4
KSJ0314 <132119447+KSJ0314@users.noreply.github.com>
2023-07-14 09:09:14 +0900

    반복연습2수정

--- test/src/test/반복연습2.java
+++ test/src/test/반복연습2.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 
 public class 반복연습2 {
 
-	public static boolean whoWin(String u, String c) {
+	public static boolean whoWin(String u, String c, String[] arr) {
 		if (u.equals("그만")) {
 			System.out.println("게임을 종료합니다.");
 			return false;
 		}
 		System.out.println("당신 : " + u + "\n컴퓨터 : " + c);
 		if (u.equals(c)) System.out.println("비겼습니다.");
-		else if (u.equals("가위") && c.equals("보") || u.equals("바위") && c.equals("가위")
-				|| u.equals("보") && c.equals("바위")) System.out.println("당신이 이겼습니다.");
+		else if (u.equals(arr[0]) && c.equals(arr[2]) || u.equals(arr[1]) && c.equals(arr[0])
+				|| u.equals(arr[2]) && c.equals(arr[1])) System.out.println("당신이 이겼습니다.");
 		else System.out.println("당신이 졌습니다.");
 		return true;
-
-	}
+		}
 
 	public static void main(String[] args) {
 		
@@ -32,7 +31,7 @@ public class 반복연습2 {
 			user = s.next();
 			compu = game[(int) (Math.random() * 3)];
 
-		} while (whoWin(user, compu));
+		} while (whoWin(user, compu, game));
 
 	}
 }
