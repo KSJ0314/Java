6f5ab69aaf8d091df43aae8fd1dd2317bb88d47e
KSJ0314 <132119447+KSJ0314@users.noreply.github.com>
2023-07-18 17:15:06 +0900

    0718

--- test/src/클래스연습문제/PhoneBook2.java
+++ test/src/클래스연습문제/PhoneBook2.java
@@ -1,4 +1,4 @@
-package test;
+package 클래스연습문제;
 
 import java.util.Scanner;
 import java.util.StringTokenizer;
--- test/src/클래스연습문제/TV.java
+++ test/src/클래스연습문제/TV.java
@@ -1,4 +1,4 @@
-package test;
+package 클래스연습문제;
 
 public class TV {
 	String company;
